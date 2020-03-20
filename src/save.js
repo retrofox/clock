deb2272955c4f743803292516774f7b253df9e07
retrofox <rdsuarez@gmail.com>
2020-03-20 17:38:24 -0300

    save: save if text has color

--- src/save.js
+++ src/save.js
@@ -1,11 +1,25 @@
 
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 
-const ClockBlockSave = () => {
+const ClockBlockSave = ( { attributes } ) => {
 
-	const cssClasses = 'wp-block-create-block-clock';
+	const {
+		textColor,
+		customTextColor,
+		backgroundColor,
+		customBackGroundColor,
+	} = attributes;
+
+	const cssClasses = classnames( 'wp-block-create-block-clock', {
+		'has-text-color': textColor || customTextColor,
+	} );
 
 	return (
 		<div className={ cssClasses }>
