c36bc883bb47dfc1aa1a8dbeb751598a51ec2eba
retrofox <rdsuarez@gmail.com>
2020-03-23 10:45:00 -0300

    declare `textColor` attribute

--- src/index.js
+++ src/index.js
@@ -30,32 +30,17 @@ registerBlockType( 'create-block/clock', {
 	 * This is the display title for your block, which can be translated with `i18n` functions.
 	 * The block inserter will show this name.
 	 */
-	title: __( 'Clock', 'create-block' ),
+	title: __( 'Clock', 'clock-block' ),
 
-	/**
-	 * This is a short description for your block, can be translated with `i18n` functions.
-	 * It will be shown in the Block Tab in the Settings Sidebar.
-	 */
 	description: __(
-		'Example block written with ESNext standard and JSX support – build step required.',
-		'create-block'
+		'Add a nice clock to your post!',
+		'clock-block'
 	),
 
-	/**
-	 * Blocks are grouped into categories to help users browse and discover them.
-	 * The categories provided by core are `common`, `embed`, `formatting`, `layout` and `widgets`.
-	 */
 	category: 'widgets',
 
-	/**
-	 * An icon property should be specified to make it easier to identify a block.
-	 * These can be any of WordPress’ Dashicons, or a custom svg element.
-	 */
 	icon: 'clock',
 
-	/**
-	 * Optional block extended support features.
-	 */
 	supports: {
 		// Removes support for an HTML mode.
 		html: false,
@@ -64,4 +49,10 @@ registerBlockType( 'create-block/clock', {
 	edit,
 
 	save,
+
+	attributes: {
+		'textColor': {
+			type: 'string',
+		},
+	}
 } );
