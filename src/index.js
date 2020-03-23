3ae3d0eb46ab17b5b8806f42a95671a9e735f60d
retrofox <rdsuarez@gmail.com>
2020-03-23 18:12:07 -0300

    clean scaffolding code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-/**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
- */
+
 import { registerBlockType } from '@wordpress/blocks';
 
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -19,16 +10,8 @@ import edit from './edit';
 import save from './save';
 import attributes from './attributes';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
- */
 registerBlockType( 'create-block/clock', {
-	/**
-	 * This is the display title for your block, which can be translated with `i18n` functions.
-	 * The block inserter will show this name.
-	 */
+
 	title: __( 'Clock', 'clock-block' ),
 
 	description: __(
@@ -39,11 +22,6 @@ registerBlockType( 'create-block/clock', {
 	category: 'widgets',
 
 	icon: 'clock',
-
-	supports: {
-		// Removes support for an HTML mode.
-		html: false,
-	},
 
 	edit,
 
